4d030874341d3e1229763d968a01fb14d3669125
Shinya YAKU <syaku@yahoo-corp.jp>
2021-06-08 21:15:45 +0900

    part 3c

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const Person = require('./models/person')
 
 var morgan = require('morgan')
 app.use(morgan('combined'))
@@ -10,6 +12,18 @@ app.use(cors())
 
 app.use(express.json())
 app.use(express.static('build'))
+
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
 
 let persons = [
   {
@@ -35,7 +49,10 @@ let persons = [
 ]
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
+  .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
@@ -55,14 +72,47 @@ app.get('/api/persons/:id', (request, response) => {
   }
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  persons = persons.filter(p => p.id != Number(request.params.id))
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
+
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: "name is missing"
+    })
+  }
+  if (!body.number) {
+    return response.status(400).json({
+      error: "number is missing"
+    })
+  }
+  Person.find({}).then(persons => {
+    if (persons.filter(p => p.name === body.name).length) {
+      return response.status(400).json({
+        error: "name must be unique"
+      })
+    }
+    const person = new Person({
+      name: body.name,
+      number: body.number
+    })
+
+    person.save().then(savedPerson => {
+      response.json(savedPerson)
+    })
+  }).catch(error => next(error))
+})
 
 app.post("/api/persons", (request, response) => {
   const body = request.body
@@ -100,3 +150,7 @@ const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
